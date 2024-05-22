de69612f03bfdbf4513b42253499a383f27466b5
Diya Agarwal <agarwal.diya2002@gmail.com>
2024-05-23 00:42:37 +0530

    Styling Flat Cards - Basic

--- components/FlatCards.tsx
+++ components/FlatCards.tsx
@@ -3,10 +3,42 @@ import React from 'react'
 
 export default function FlatCards() {
   return (
-    <View>
+    <view>
+    <view style={styles.headingText}>
       <Text>FlatCards</Text>
-    </View>
+    </view>
+    <view>
+        <view style={styles.one}>
+          <Text>
+              Red
+          </Text>
+        </view>
+        <view style={styles.two}>
+          <Text>
+              Red
+          </Text>
+        </view>
+        <view style={styles.three}>
+          <Text>
+              Red
+          </Text>
+        </view>
+    </view>
+    </view>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  headingText:{
+fontSize : 24
+  },
+  one:{
+backgroundColor:'red'
+  },
+  two:{
+backgroundColor:'green'
+  },
+  three:{
+backgroundColor:'blue'
+  }
+})
